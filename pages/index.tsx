f198c40cd9db17976cf2588af4e067c8c11f4a52
oyoops <121578862+oyoops@users.noreply.github.com>
2023-05-02 17:24:08 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,10 +11,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Next.js OpenAI Template</title>
+        <title>MuniGPT, Powered by oyoopsAI</title>
         <meta
           name="description"
-          content="Next.js Template for building OpenAI applications with Supabase."
+          content="Ask me anything about development regulations in Broward County, FL. Powered by oyoopsAI."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
@@ -26,16 +26,16 @@ export default function Home() {
 
         <div className="py-8 w-full flex items-center justify-center space-x-6">
           <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
-            <Link href="https://supabase.com" className="flex items-center justify-center">
-              <p className="text-base mr-2">Built by Supabase</p>
-              <Image src={'/supabase.svg'} width="20" height="20" alt="Supabase logo" />
+            <Link href="https://oyoops.com" className="flex items-center justify-center">
+              <p className="text-base mr-2">Built by @oyoops</p>
+              <Image src={'/supabase.svg'} width="20" height="20" alt="oyoops logo" />
             </Link>
           </div>
           <div className="border-l border-gray-300 w-1 h-4" />
           <div className="flex items-center justify-center space-x-4">
             <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
               <Link
-                href="https://github.com/supabase/supabase"
+                href="https://github.com/oyoops/MuniGPT-beta"
                 className="flex items-center justify-center"
               >
                 <Image src={'/github.svg'} width="20" height="20" alt="Github logo" />
@@ -43,7 +43,7 @@ export default function Home() {
             </div>
             <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
               <Link
-                href="https://twitter.com/supabase"
+                href="https://twitter.com/oyoops"
                 className="flex items-center justify-center"
               >
                 <Image src={'/twitter.svg'} width="20" height="20" alt="Twitter logo" />
