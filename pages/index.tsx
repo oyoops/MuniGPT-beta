562e81d773094147de809519cf83567f2ebd918d
NS <121578862+oyoops@users.noreply.github.com>
2023-05-08 15:56:38 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,43 +8,39 @@ import Link from 'next/link'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-return (
-<>
-<Head>
-<title>MuniGPT (beta)</title>
-</br>
-</br>
-<meta
-       name="description"
-       content="Ask me anything about zoning/land use regulations or the building code in Broward County, FL."
-     />
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<link rel="icon" href="/favicon.ico" />
-</Head>
-<main className={styles.main}>
-<div className={styles.center}>
-<h1 className={styles.title}>MuniGPT</h1>
-<p className={styles.description}>
-</br>
-<b>MuniGPT</b> is an AI-powered tool designed to answer questions about zoning, land use regulations, and the building code in Broward County, FL. Please note that this is a beta version and may not always provide accurate or complete information. Always consult official sources for the most accurate and up-to-date information.
-</br>
-</p>
-<SearchDialog />
-</div>
-<div className="py-8 w-full flex items-center justify-center space-x-6">
-<div className="border-l border-gray-300 w-1 h-4" />
-<div className="flex items-center justify-center space-x-4">
-<div className="opacity-75 transition hover:opacity-100 cursor-pointer">
-<Link
-             href="https://oyoops.com"
-             className="flex items-center justify-center"
-           >
-<Image src={'/supabase.svg'} width="20" height="20" alt="oyoops logo" />
-</Link>
-</div>
-</div>
-</div>
-</main>
-</>
-)
+  return (
+    <>
+      <Head>
+        <title>MuniGPT (beta)</title>
+        <meta
+          name="description"
+          content="Ask me anything about zoning/land use regulations or the building code in Broward County, FL."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.center}>
+          <h1 className={`${styles.title} text-4xl mb-4`}>MuniGPT</h1>
+          <p className={`${styles.description} mb-4`}>
+            MuniGPT is an AI-powered tool designed to answer questions about zoning, land use regulations, and the building code in Broward County, FL. Please note that this is a beta version and may not always provide accurate or complete information. Always consult official sources for the most accurate and up-to-date information.
+          </p>
+          <SearchDialog />
+        </div>
+        <div className="py-8 w-full flex items-center justify-center space-x-6">
+          <div className="border-l border-gray-300 w-1 h-4" />
+          <div className="flex items-center justify-center space-x-4">
+            <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
+              <Link
+                href="https://oyoops.com"
+                className="flex items-center justify-center"
+              >
+                <Image src={'/supabase.svg'} width="20" height="20" alt="oyoops logo" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  )
 }
