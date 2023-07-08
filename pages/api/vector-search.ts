d8496ffd69e89b7839d3591cfbb694de5ae6d1d9
NS <121578862+oyoops@users.noreply.github.com>
2023-07-07 21:52:58 -0400

    Update vector-search.ts

--- pages/api/vector-search.ts
+++ pages/api/vector-search.ts
@@ -136,8 +136,7 @@ export default async function handler(req: NextRequest) {
     `
 
     const completionOptions: CreateCompletionRequest = {
-      //model: 'text-davinci-003',
-      model: 'gpt-3.5-turbo',
+      model: 'text-davinci-003',
       prompt,
       max_tokens: 1024,
       temperature: 0.1,
